9ded49c4df918905edeec0e588bcd7a42c3f0a5a
John <xzten3@gmail.com>
2017-09-22 21:55:19 -0600

    modified existing tests

---
+++ team_four/src/test/java/edu/msudenver/cs/team_four/UserServiceTests.java
@@ -0,0 +1,17 @@
+package edu.msudenver.cs.team_four;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import edu.msudenver.cs.team_four.User.User;
+import edu.msudenver.cs.team_four.User.UserController;
+import edu.msudenver.cs.team_four.User.UserService;
+
+public class UserServiceTests {
+	static User testerUser1;
+	static User testerUser2;
+	static User testerUser3;
+	static UserController testerUC;
+	static UserService testerUS;
+}
