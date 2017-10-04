890da23607d6a4b0a6163abb9e867b60dcf36b74
John <xzten3@gmail.com>
2017-10-04 00:05:19 -0600

    Added LayoutController for html formating and a few new tabs in nav

---
+++ team_four/src/main/java/edu/msudenver/cs/team_four/User/LayoutController.java
@@ -0,0 +1,44 @@
+package edu.msudenver.cs.team_four.User;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LayoutController {
+	
+	public String HEADER = 
+			"<!DOCTYPE HTML>\r\n" + 
+			"<html>\r\n" + 
+			"	<head>\r\n" + 
+			"		<title>MSU Denver</title>\r\n" + 
+			"		<link rel=\"icon\" href=\"logo_handler.png\" />\r\n" + 
+			"		<meta charset=\"utf-8\">\r\n" + 
+			"  		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
+			"	  	<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">\r\n" + 
+			"  		<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>\r\n" + 
+			"  		<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>\r\n" + 
+			"	</head>\r\n" + 
+			"	<body>\r\n" + 
+			"		<nav class=\"navbar navbar-inverse\">\r\n" + 
+			"  			<div class=\"container-fluid\">\r\n" + 
+			"    			<div class=\"navbar-header\">\r\n" + 
+			"      				<a class=\"navbar-brand\" href=\"/\">MSU Denver</a>\r\n" + 
+			"    			</div>\r\n" + 
+			"    			<ul class=\"nav navbar-nav\">\r\n" + 
+			"      				<li><a href=\"/Students\">Students</a></li>\r\n" + 
+			"					<li><a href=\"/Professors\">Professors</a></li>\r\n" +
+			"      				<li><a href=\"/Courses\">Courses</a></li>\r\n" + 
+			"					<li><a href=\"/Sections\">Sections</a></li>\r\n" +
+			"			    </ul>\r\n" + 
+			"  			</div>\r\n" + 
+			"		</nav>";
+	public String BODY = 
+			"	</body>" +
+			"</html>";
+	
+	@RequestMapping("/")
+	public String home() {
+		return HEADER + BODY;
+	}
+
+}
