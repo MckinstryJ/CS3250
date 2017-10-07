a61e2b2569823527b8a2f555c73437c75e64791f
John <xzten3@gmail.com>
2017-10-07 15:21:06 -0600

    not completed form submit

--- team_four/src/main/java/edu/msudenver/cs/team_four/models/StudentController.java
+++ team_four/src/main/java/edu/msudenver/cs/team_four/models/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,11 +26,9 @@ public class StudentController {
 
 	@RequestMapping("/Students")
 	public String getAllStudents(){
-		//AS AN EXAMPLE
 		Student stu = new Student("123456", "John Mckinstry");
 		studentService.addStudent(stu);
 		
-		//How we will display student info through table...
 		String studentList = "";
 		for (Student s : studentService.getAllStudents()) {
 			studentList += "<tr><th>" + s.getId() + "</th>" 
@@ -41,25 +40,43 @@ public class StudentController {
 	}
 	
 	@RequestMapping("/Students/{id}")
-	public Student getStudent(@PathVariable String id) {
+	public String getStudent(@PathVariable String id) {
 		Student stu = studentService.getStudent(id);
 		
-		/*
-		 * Show Student's complete info... name, id, courses registered, PRE-req met?
-		 * Offer to update student's name/id or delete student.
-		 */
+		String studentForm = "<form name=\"stuInfo\" action=\"stuInfo\" method=\"PUT\" class=\"bg-info\" style=\"width:80%;margin:auto;padding:20px;>"
+				+ "			<div class=\"form-group\">" 
+				+ "				<label for=\"studentID\">Student Id:</label>"
+				+ "				<input name=\"id\" type=\"text\" class=\"form-control\" id=\"studentID\" placeholder=\"" 
+									+ stu.getId() + "\">"
+				+ "			</div>"
+				+ "			<div class=\"form-group\">"
+				+ "				<label for=\"studentName\"><br>Name:</label>"
+				+ "				<input name=\"newName\" type=\"text\" class=\"form-control\" id=\"studentName\" placeholder=\"" 
+									+ stu.getName() + "\">"
+				+ "			</div>"
+				+ "			<button type=\"submit\" class=\"btn btn-primary\">Update</button>"
+				+ "			<button type=\"delete\" class=\"btn btn-primary\">Delete</button>"
+				+ "		</form>";
 		
-		return null;
+		return LayoutController.HEADER + studentForm + LayoutController.BODY;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/Students")
-	public void addStudent(@RequestBody Student Student) {
-		studentService.addStudent(Student);
+	public void addStudent(@RequestBody Student student) {
+		studentService.addStudent(student);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/Students/{id}")
-	public void updateStudent(@RequestBody Student Student, @PathVariable String id) {
-		studentService.updateStudent(id, Student);
+	@RequestMapping(method=RequestMethod.PUT, value="/stuInfo")
+	public String updateStudent(@PathVariable String id, @RequestParam("newName") String newName) {
+		Student stu = studentService.getStudent(id);
+		if (newName != null) {
+			stu.setName(newName);
+			studentService.updateStudent(id, stu);
+		} else {
+			//???
+		}
+		
+		return getStudent(id);
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE, value="/Students/{id}")
