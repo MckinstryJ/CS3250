2807d40a632a96313233374086202739e0344f57
John <xzten3@gmail.com>
2017-10-09 20:22:44 -0600

    overhalling the project --- current base

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5/10ed3dcb58ad001711c59ca49cb54277
@@ -0,0 +1,12 @@
+package edu.msudenver.cs.team_four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFourApplication.class, args);
+	}
+}
--- team_four/src/main/java/edu/msudenver/cs/team_four/TeamFourApplication.java
+++ team_four/src/main/java/edu/msudenver/cs/team_four/TeamFourApplication.java
@@ -3,6 +3,10 @@ package edu.msudenver.cs.team_four;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
 @SpringBootApplication
 public class TeamFourApplication {
 
