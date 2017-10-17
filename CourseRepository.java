e7a38c110e10f8daafcad5695ab9e3de67a8fcb2
John <xzten3@gmail.com>
2017-10-16 22:56:10 -0600

    clean up main

--- CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.msudenver.cs.team_four.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Courses, String>{
-
-}
