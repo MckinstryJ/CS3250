ca9fd29128e9dcd336b0c2bd8b1028737e8e6aa6
afflaic <31318645+afflaic@users.noreply.github.com>
2017-10-16 21:49:45 -0600

    Some test updates to fix the errors
    I couldn't remember exactly which files I changed so I just grabbed them all. This should fix a lot of the test errors I got from the latest build.

---
+++ StudentControllerTest.java
@@ -0,0 +1,17 @@
+package edu.msudenver.cs.team_four;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentControllerTest {
+
+	/*
+	 * Leaving this alone for now...
+	 */
+	@Test
+	public void getallStudentsTest() {
+		
+	}
+
+}
