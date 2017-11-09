11b636bb231be9ddb6f2928da85f3d8271c8a9c3
Joel Martinez <joelm1011@yahoo.com>
2017-11-09 08:36:06 -0700

    Update Buildings.java

--- team_four/src/main/java/edu/msudenver/cs/team_four/models/Buildings.java
+++ team_four/src/main/java/edu/msudenver/cs/team_four/models/Buildings.java
@@ -1,29 +1,49 @@
 package edu.msudenver.cs.team_four.models;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Buildings {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id;
+    private long id = 123456789;
 
-    @Column(unique=true)
-    private String buildingName;
+    private String buildingName = "DEFAULT";
+    private int numberOfRooms = 1;
 
-    @Column(unique = true)
-    private String address;
+    @OneToOne
+    @JoinColumn(name = "room_id")
+    private Rooms rooms;
 
-    @Column(unique = true)
-    private String acronym;
+    protected Buildings() {
 
-    public void setId(String id){
-
-        this.id=id;
     }
 
-    public String getbuildingName() {
+    public Buildings(String buildingName) {
+
+        setBuildingName(buildingName);
+    }
+
+    public Buildings(String buildingName, int numberOfRooms) {
+
+        this.buildingName = buildingName;
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public Buildings(String buildingName, int numberOfRooms, long buildingId) {
+
+        setBuildingName(buildingName);
+        setNumberOfRooms(numberOfRooms);
+        setId(buildingId);
+    }
+
+    public String getBuildingName() {
+
         return buildingName;
     }
 
@@ -32,27 +52,32 @@ public class Buildings {
         this.buildingName = buildingName;
     }
 
-    public String getAddress() {
+    public int getNumberOfRooms() {
 
-        return address;
+        return numberOfRooms;
     }
 
-    public void setAddress(String address) {
+    public void setNumberOfRooms(int numberOfRooms) {
 
-        this.address = address;
+        this.numberOfRooms = numberOfRooms;
     }
 
-    public String getAcronym() {
-        return acronym;
-    }
-
-    public void setAcronym(String acronym) {
-        this.acronym = acronym;
-    }
-    
-    public String getId() {
+    public long getId() {
 
         return id;
     }
 
+    public void setId(long id) {
+
+        this.id = id;
+    }
+
+    public Rooms getRoom() {
+
+        return rooms;
+    }
+
+    public void setRoom(int roomNumber, int numberOfSeats) {
+
+    }
 }
