2ec5b4b4e972c46e0b9c0e5da05a3e4b0b10ee2d
CodeVII <d_medina_8@yahoo.com>
2017-11-24 11:02:19 -0700

    This should have a null pointer exception
    Im not sure why Travis CI would not accept this, this should have a null pointer exception expected.

---
+++ src/test/java/edu/msudenver/cs/team_four/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package edu.msudenver.cs.team_four;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HomeControllerTest {
+	/*
+	 * The Test monster was here...
+	 */
+	@Test  (expected = NullPointerException.class)
+	public void showHomeTest() {
+		
+	}
+}
