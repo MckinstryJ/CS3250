84bc40ace0ecb2bb48558c6c15cb37a04fd9ab92
John <xzten3@gmail.com>
2017-12-02 16:42:14 -0700

    setup for custom CourseView

--- src/test/java/edu/msudenver/cs/team_four/CoursesTest.java
+++ src/test/java/edu/msudenver/cs/team_four/CoursesTest.java
@@ -63,42 +63,5 @@ public class CoursesTest {
 		courseTester.setLearningObjectives("Learn computer shit");
 		assertEquals("Learn computer shit", courseTester.getLearningObjectives());
 	}
-	
-	@Test
-	public void setCourseSemester() {
-		String[] semester = {"Spring","Spring","Spring"};
-		courseTester.setSemester(semester);
-		assertEquals("Spring", courseTester.getSemester()[0]);
-	}
-	@Test
-	public void setCourseDates() {
-		String[] dates = {"15","23","21"};
-		courseTester.setDates(dates);
-		assertEquals("23", courseTester.getDates()[1]);
-	}
-	@Test
-	public void setCourseBuilding() {
-		String[] buildings = {"North","South","East"};
-		courseTester.setBuilding(buildings);
-		assertEquals("East", courseTester.getBuilding()[2]);
-	}
-	@Test
-	public void setCourseRoom() {
-		String[] rooms = {"101","102","103"};
-		courseTester.setRoom(rooms);
-		assertEquals("101", courseTester.getRoom()[0]);
-	}
-	@Test
-	public void setCourseDay() {
-		String[] days = {"M", "T", "F"};
-		courseTester.setDay(days);
-		assertEquals("F", courseTester.getDay()[2]);
-	}
-	@Test
-	public void setCourseTime() {
-		String[] times = {"8","10","12"};
-		courseTester.setTime(times);
-		assertEquals("10", courseTester.getTime()[1]);
-	}
 }
 
--- src/test/java/edu/msudenver/cs/team_four/ProfessorServiceTests.java
+++ src/test/java/edu/msudenver/cs/team_four/ProfessorServiceTests.java
@@ -2,59 +2,25 @@ package edu.msudenver.cs.team_four;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 import edu.msudenver.cs.team_four.models.Professor;
 import edu.msudenver.cs.team_four.models.ProfessorController;
+import edu.msudenver.cs.team_four.models.ProfessorRepository;
 import edu.msudenver.cs.team_four.models.ProfessorService;
 
 public class ProfessorServiceTests {
-	static Professor testerProfessor1 = new Professor("111112", "Bob","lastName");
-	static Professor testerProfessor2 = new Professor("121212","Mick","lastName");
-	static Professor testerProfessor3 = new Professor("987654","Jane","lastName");
-	static ProfessorController testerUC;
-	static ProfessorService testerUS;
-	static String testerId;
 	
-	@Test  (expected = NullPointerException.class)
+	@Mock
+	Professor professor;
+	
+	@Mock
+	private ProfessorService proService;
+	
+	@Test  
 	public void addProfessorTest() {
-		testerUS = new ProfessorService();
-		testerUS.addProfessor(testerProfessor1);
-		testerUS.addProfessor(testerProfessor2);
-		testerUS.addProfessor(testerProfessor3);
-		assertEquals(3, testerUS.getAllProfessors().size());
-	}
-	
-	@Test  (expected = NullPointerException.class)
-	public void getProfessorTest() {
-		testerProfessor1 = new Professor("123456","Bill","lastName");
-		testerUS = new ProfessorService();
-		testerUS.addProfessor(testerProfessor1);
-		assertFalse(testerUS.getProfessor("123456").toString().isEmpty());
-	}
-	
-	@Test  (expected = NullPointerException.class)
-	public void getProfessorNameTest() {
-		testerProfessor1 = new Professor("123456","Bill","lastName");
-		testerUS = new ProfessorService();
-		testerUS.addProfessor(testerProfessor1);
-		assertEquals("Bill", testerUS.getProfessor("123456"));
-	}
-	
-	@Test  (expected = NullPointerException.class)
-	public void updateProfessorTest() {
-		testerProfessor1 = new Professor("123456","Bill","lastName");
-		testerProfessor2 = new Professor("123456","Shetern","lastName");
-		testerUS = new ProfessorService();
-		testerUS.addProfessor(testerProfessor1);
-		testerUS.updateProfessor(testerProfessor2);
-		assertEquals("Shetern", testerUS.getProfessor("123456"));
-	}
-	
-	@Test  (expected = NullPointerException.class)
-	public void deleteProfessorTest() {
-		testerProfessor1 = new Professor("123456","Bill","lastName");
-		testerUS = new ProfessorService();
-		testerUS.addProfessor(testerProfessor1);
-		testerUS.deleteProfessor(testerId);
-		assertTrue(testerUS.getAllProfessors().isEmpty());
+		
 	}
 }
