4b1351e10e4aa392fbe295e895151875acdc911c
Joel Martinez <joelm1011@yahoo.com>
2017-12-06 22:45:16 -0700

    Update ProfessorControllerTest.java
    Not much but I haven't been able to move forward with this..

--- src/test/java/edu/msudenver/cs/team_four/ProfessorControllerTest.java
+++ src/test/java/edu/msudenver/cs/team_four/ProfessorControllerTest.java
@@ -1,22 +1,32 @@
 package edu.msudenver.cs.team_four;
 
-import static org.junit.Assert.assertEquals;
-
+import edu.msudenver.cs.team_four.models.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import edu.msudenver.cs.team_four.models.CourseController;
-import edu.msudenver.cs.team_four.models.CourseService;
-import edu.msudenver.cs.team_four.models.Courses;
-import edu.msudenver.cs.team_four.models.Professor;
-import edu.msudenver.cs.team_four.models.ProfessorController;
-import edu.msudenver.cs.team_four.models.ProfessorService;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProfessorControllerTest {
+	@Autowired
+	private ProfessorController controller;
 	
-
+	@Autowired
+	ProfessorRepository professorRepository;
+	
+	@Test
+	public void testContextLoads() throws Exception{
+		assertThat(controller).isNotNull();
+		
+	}
+	
+	@Test
+	public void testGetAllProfessors() throws Exception{
+	
+	
+	} 
 }
 
